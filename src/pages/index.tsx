069c6a5d8031accc70dbb47e5463e60cfb647e2c
Brandon Bothell <bothellbrandon@gmail.com>
2023-07-28 02:27:54 -0400

    The beginning of the end

---
+++ src/pages/index.tsx
@@ -0,0 +1,86 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { api } from "~/utils/api";
+
+export default function Home() {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <Head>
+        <title>Login Example</title>
+        <meta name="description" content="An example login application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            Basic <span className="text-[hsl(280,100%,70%)]">Login</span> App
+          </h1>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="https://github.com/brandonbothell"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">GitHub →</h3>
+              <div className="text-lg">
+                Check out my GitHub - you can find everything
+                I&apos;ve ever publicly made here, including this
+                very program, for free.
+              </div>
+            </Link>
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="https://github.com/brandonbothell/popyt"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">Popyt →</h3>
+              <div className="text-lg">
+                Use Popyt for accessing data from the YouTube Data v3 API.
+                It&apos;s fast, simple, and has caching & pagination.
+              </div>
+            </Link>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-2xl text-white">
+              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+            </p>
+            <AuthShowcase />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+function AuthShowcase() {
+  const { data: sessionData } = useSession();
+
+  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined }
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-white">
+        {sessionData && <span>Logged in as {sessionData.user?.image ?
+          <Image
+            className="rounded-full inline profile-pic ring-2"
+            src={sessionData.user?.image} width={30} height={30}
+            alt="Profile picture"
+          /> : null}{sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      <button
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+}
