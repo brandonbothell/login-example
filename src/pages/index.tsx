f94fcaf3d87bbca6ec71a20ddafa4446dc02be1e
Brandon Bothell <bothellbrandon@gmail.com>
2026-04-20 17:22:03 -0400

    fix: update links on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://github.com/brandonbothell/popyt"
+              href="https://popyt.brandonsbench.net"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">Popyt →</h3>
@@ -45,7 +45,7 @@ export default function Home() {
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://github.com/brandonbothell"
+              href="https://github.com/brandonbothell/gander"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">Gander →</h3>
